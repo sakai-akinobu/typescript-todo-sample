9e77f3f3f0d0ff772124c8b762fd44c7fdb4f773
sakai-akinobu <sakai.akinobu@gmail.com>
2018-09-30 09:17:12 +0900

    Compile with Babel.

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: '>= 0.1%',
+      useBuiltIns: 'usage'
+    }],
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
